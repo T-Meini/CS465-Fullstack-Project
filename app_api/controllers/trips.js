aab7bd9eddfd725452cd20c3f9e6b43ea231f896
T-Meini <Meininger999@gmail.com>
2024-08-10 18:18:04 -0400

    Module 6 Completed Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -10,7 +10,7 @@ const tripsList = async(req, res) => {
         .find({}) // No filter, return all records
         .exec();
 
-        console.log(q);
+        //console.log(q);
 
     if(!q)
     { // Database returned no data
@@ -48,7 +48,78 @@ const tripsFindByCode = async(req, res) => {
 
 };
 
+//POST: /trips - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+        if (!q)
+        { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else { // Return new trip
+            return res
+                .status(201)
+                .json(q);
+        }
+        
+        //console.log(q);
+};
+
+// PUT: /trips/:tripCode - Updates a Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+    //console.log(req.params);
+    //console.log(req.body);
+    try {
+      const q = await Model.findOneAndUpdate(
+        { code: req.params.tripCode },
+        {
+          code: req.body.code,
+          name: req.body.name,
+          length: req.body.length,
+          start: req.body.start,
+          resort: req.body.resort,
+          perPerson: req.body.perPerson,
+          image: req.body.image,
+          description: req.body.description,
+        }
+      ).exec();
+  
+      if (!q) {
+        // Database returned no data
+        return res.status(404).json({ message: "Trip not found" });
+      } else {
+        // Return resulting updated trip
+        return res.status(201).json(q);
+      }
+  
+      // Uncomment the following line to show results of operation
+      // console.log(q);
+    } catch (error) {
+      console.error("Error updating trip:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
